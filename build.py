a13415fc300b67676a7bd63a97f38a728e47944d
t3dy <ted.hand@gmail.com>
2026-06-14 23:38:54 -0700

    Remove Arcanvm and Society of Antiquaries from Featured Podcasts toolbar

--- build.py
+++ build.py
@@ -522,7 +522,8 @@ def render(catalog, cfg, now):
     not_a_podcast = {"tarotsoulless", "dzwiza", "kazrowe", "bytesizescience",
                      "michaelkuhlman", "greshamhutton", "joekiernan",
                      "cafeneaua_newman", "spiritus_mundi_newman", "adam_mclean",
-                     "fraserbuilds_alchemy", "wabt", "maeviuslynn", "historyofalchemy"}
+                     "fraserbuilds_alchemy", "wabt", "maeviuslynn", "historyofalchemy",
+                     "arcanvm", "sas_lectures"}
     global FEATURED_SRC_IDS, SCHOLAR_TABS
     FEATURED_SRC_IDS = {s["id"] for s in sources
                         if src_counts.get(s["id"], 0) > 0 and s["id"] not in not_a_podcast}
